5ca409a102a6bc6fddfc1792acecaa62319102f5
Amritanshu312 <ffdarkrayon@gmail.com>
2024-02-19 22:25:14 +0800

    fix: google search tag

--- app/layout.js
+++ app/layout.js
@@ -2,21 +2,20 @@ import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/layout/footer/Footer";
 import Navbar from "@/components/layout/navbar/Navbar";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: `${process.env.WEBSITE_NAME} || Watch TV Shows Online, Watch Movies Online`,
   description: `Explore thousands of free shows on ${process.env.WEBSITE_NAME}. Enjoy a sleek interface and uncover hidden gems and classic favorites. Relax and unwind with our diverse selection.`,
+  robots: { index: true, follow: false },
+  verification: { google: 'ls1OUoOoLjxYsmKMPQ1ML9P99TWDsm7d5hfnGQjW7Tw', }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-site-verification" content="ls1OUoOoLjxYsmKMPQ1ML9P99TWDsm7d5hfnGQjW7Tw" />
-      </Head>
+
       <body className={inter.className}>
         <Navbar />
         {children}
