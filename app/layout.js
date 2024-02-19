507721e03c51b160a69d242dcf841a613f116cee
Amritanshu312 <ffdarkrayon@gmail.com>
2024-02-19 22:30:06 +0800

    fix: google search tag 2

--- app/layout.js
+++ app/layout.js
@@ -8,8 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: `${process.env.WEBSITE_NAME} || Watch TV Shows Online, Watch Movies Online`,
   description: `Explore thousands of free shows on ${process.env.WEBSITE_NAME}. Enjoy a sleek interface and uncover hidden gems and classic favorites. Relax and unwind with our diverse selection.`,
-  robots: { index: true, follow: false },
-  verification: { google: 'ls1OUoOoLjxYsmKMPQ1ML9P99TWDsm7d5hfnGQjW7Tw', }
+  // verification: { google: 'ls1OUoOoLjxYsmKMPQ1ML9P99TWDsm7d5hfnGQjW7Tw', }
+  other: {
+    "X-Robots-Tag": "index, nofollow",
+    'google-site-verification': 'ls1OUoOoLjxYsmKMPQ1ML9P99TWDsm7d5hfnGQjW7Tw',
+    "X-Frame-Options": "SAMEORIGIN",
+  }
 };
 
 export default function RootLayout({ children }) {
